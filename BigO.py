b6109ff01d93a87b40ee6ff9649fd8ccbb758bc5
anthonyc39 <colajezzi.anthony@gmail.com>
2026-01-19 15:49:49 -0500

    Revert BigO script to original state

---
+++ BigO.py
@@ -0,0 +1,10 @@
+# BigO Indicator
+
+class BigO:
+    def calculate(self, data):
+        # Original algorithm logic goes here
+        pass
+
+    def visualize(self):
+        # Visualization logic goes here
+        pass
